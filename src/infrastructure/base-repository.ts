06151dd85607c1ce107ad17c264b69e68be870f2
Koske Kimutai <koskedk@live.com>
2018-10-10 23:50:36 +0300

    adding rethinkdb

--- src/infrastructure/base-repository.ts
+++ src/infrastructure/base-repository.ts
@@ -1,9 +1,12 @@
 import { IRepository } from "../interfaces/irepository";
 import { Entity } from "../model/entity";
+import * as r from "rethinkdb";
+import { Connection } from "rethinkdb";
 
 
 export abstract class BaseRepository<T extends Entity<TId>, TId> implements IRepository<T, TId> {
 
+    connection: Connection;
     entities: Array<T>;
 
     constructor() {
@@ -27,5 +30,12 @@ export abstract class BaseRepository<T extends Entity<TId>, TId> implements IRep
         if (e) {
             this.entities.splice(this.entities.indexOf(e), 1);
         }
+    }
+
+    connnectDb(): void {
+        r.connect({host: "127.0.0.1", port: 28015}, (err, conn) => {
+            if (err) throw err;
+            this.connection = conn;
+        });
     }
 }
